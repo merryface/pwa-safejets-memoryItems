62d4e522d544698bf3a8c70c1a96fb4dfff3a686
Tarik <tarikmerrylees@gmail.com>
2023-06-15 19:29:15 +0200

    add card and style

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@ const assets = [
   'https://fonts.googleapis.com/icon?family=Material+Icons',
   'https://fonts.gstatic.com/s/materialicons/v140/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
 ]
-
+//
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(staticCacheName).then(cache => {
