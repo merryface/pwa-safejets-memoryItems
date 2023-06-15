efabcfc5d3693ebc3fdc0bb90e572eaf5ca54e42
Tarik <tarikmerrylees@gmail.com>
2023-06-15 17:31:07 +0200

    add install and activate listeners

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-if('ServiceWorker' in navigator){
+if('serviceWorker' in navigator){
     navigator.serviceWorker.register('/sw.js')
     .then(reg => console.log('Service worker registered:', reg))
-    .catch(err => console.warn('Error registering service worker:', err))
+    .catch(err => console.log('Error registering service worker:', err))
 }
