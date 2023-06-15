f132a2d8661be1d9f23543dbe9852f22c35f0e53
Tarik <tarikmerrylees@gmail.com>
2023-06-15 18:36:04 +0200

    cache site

--- sw.js
+++ sw.js
@@ -1,11 +1,31 @@
+const staticCacheName = 'site-static';
+const assets = [
+  '/',
+  '/index.html',
+  '/pages/latitude.html',
+  '/pages/xls.html',
+  '/pages/phenom.html',
+  '/js/app.js',
+  '/js/ui.js',
+  '/js/materialize.min.js',
+  '/css/styles.css',
+  '/css/materialize.min.css',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+]
+
 self.addEventListener('install', function(event) {
-  console.log('Service worker installed');
+  event.waitUntil(
+    caches.open(staticCacheName).then(cache => {
+      console.log('caching shell assets');
+      cache.addAll(assets)
+    })
+  )
 });
 
 self.addEventListener('activate', function(event) {
-  console.log('Service worker activated');
+  // console.log('Service worker activated');
 });
 
 self.addEventListener('fetch', function(event) {
-  console.log('fetch event', event);
+  // console.log('fetch event', event);
 });
