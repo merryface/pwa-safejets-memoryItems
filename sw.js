ef1c40c78f3c56a639267b03b4ad661cc47d149e
Tarik <tarikmerrylees@gmail.com>
2023-06-15 18:06:00 +0200

    fetch event

--- sw.js
+++ sw.js
@@ -5,3 +5,7 @@ self.addEventListener('install', function(event) {
 self.addEventListener('activate', function(event) {
   console.log('Service worker activated');
 });
+
+self.addEventListener('fetch', function(event) {
+  console.log('fetch event', event);
+});
